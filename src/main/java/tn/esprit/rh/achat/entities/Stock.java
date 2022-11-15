d2f754aeb89fedc008084c4c721ec707f31d8a59
romdhane.moadh <moadh.romdhane@esprit.tn>
2022-11-15 22:09:37 +0100

    Modification stock

--- src/main/java/tn/esprit/rh/achat/entities/Stock.java
+++ src/main/java/tn/esprit/rh/achat/entities/Stock.java
@@ -12,7 +12,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+//thiscommentismadetotestthegitupdatefeature
 @Entity
 @Getter
 @Setter
